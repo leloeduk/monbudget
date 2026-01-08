da7fdfe97a9846e9ece0155860ee56ba5cf848b4
leloeduk <leloeduk2025@gmail.com>
2026-01-08 01:24:52 +0100

    feat(expense) : ajout du type et de la date dans l’entité Expense

--- lib/domain/entities/expense.dart
+++ lib/domain/entities/expense.dart
@@ -1,6 +1,18 @@
+// ignore: constant_identifier_names
+enum ExpenseCategory { INVESTMENT, CHARGES, PLEASURE }
+
 class Expense {
+  final String id;
   final String title;
   final double amount;
+  final ExpenseCategory category;
+  final DateTime date;
 
-  Expense({required this.title, required this.amount});
+  Expense({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+  });
 }
