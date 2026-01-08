cdfc4ff7254cc28938935aa7ad432be0e43bb820
leloeduk <leloeduk2025@gmail.com>
2026-01-08 01:25:11 +0100

    feat(bloc) : ajout des événements de dépense

--- lib/presentation/bloc/expense_event.dart
+++ lib/presentation/bloc/expense_event.dart
@@ -1,19 +1,36 @@
+import 'package:monbudget/domain/entities/expense.dart';
+
 abstract class ExpenseEvent {}
 
-// chargement
 class LoadExpenseEvent extends ExpenseEvent {}
 
-// faire ajout
 class AddExpenseEvent extends ExpenseEvent {
   final String title;
   final double amount;
+  final ExpenseCategory category;
+  final DateTime date;
 
-  AddExpenseEvent({required this.title, required this.amount});
+  AddExpenseEvent({
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+  });
 }
 
-// faire la suppression
 class DeleteExpenseEvent extends ExpenseEvent {
   final int index;
 
   DeleteExpenseEvent({required this.index});
+}
+
+class LoadAnalyticsEvent extends ExpenseEvent {
+  final DateTime start;
+  final DateTime end;
+  LoadAnalyticsEvent({required this.start, required this.end});
+}
+
+class FilterByPeriodEvent extends ExpenseEvent {
+  final String period; // 'day'|'week'|'month'|'year'
+  FilterByPeriodEvent(this.period);
 }
